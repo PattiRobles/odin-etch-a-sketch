48d61548d71115c497791acc6b9f3a41d45d309e
Patricia Robles <122781084+PattiRobles@users.noreply.github.com>
2024-12-12 19:05:46 +0000

    adds reset functionality

--- script.js
+++ script.js
@@ -1,48 +1,47 @@
 
-const drawButton = document.getElementById('drawButton');
+const initialDiv = document.getElementById('initial-div');
 const body = document.body;
 const coloredCells = document.querySelectorAll('.color')
 
+const grid = document.createElement('div');
+grid.classList.add('container-grid');
+body.classList.add('centered');
+initialDiv.insertAdjacentElement('afterend', grid);
 
-drawButton.addEventListener('click', () => {
-  const grid = document.createElement('div');
-  grid.classList.add('container-grid');
-  body.classList.add('centered');
-  drawButton.insertAdjacentElement('afterend', grid);
+for (let i = 1; i <= (16*16); i++) {
+  const cell = document.createElement('div')
+  cell.classList.add('cell');
+  console.log(`Div ${i}`);
+  grid.appendChild(cell)
+  cell.addEventListener('mouseenter', () => {
+    cell.classList.add('color')
+  });
+}
 
-  for (let i = 1; i <= (16*16); i++) {
-    const cell = document.createElement('div')
-    cell.classList.add('cell');
-    console.log(`Div ${i}`);
-    grid.appendChild(cell)
-    cell.addEventListener('mouseenter', () => {
-      cell.classList.add('color')
-    });
+const buttonContainer = document.createElement('div');
+buttonContainer.classList.add('button-container')
+
+const eraseButton = document.createElement('button');
+eraseButton.textContent = 'ERASE';
+eraseButton.classList.add('erase');
+
+const resetButton = document.createElement('button');
+resetButton.textContent = 'RESET';
+resetButton.classList.add('reset')
+
+buttonContainer.append(eraseButton, resetButton)
+grid.insertAdjacentElement('afterend', buttonContainer);
+
+resetButton.addEventListener('click', () => {
+  console.log('erase button clicked')
+  cells = document.querySelectorAll('.cell')
+  for ( let cell of cells) {
+      cell.classList.remove('color')
+    }
   }
-  drawButton.remove();
+)
 
-  const buttonContainer = document.createElement('div');
-  buttonContainer.classList.add('button-container')
 
-  const eraseButton = document.createElement('button');
-  eraseButton.textContent = 'ERASE';
-  eraseButton.classList.add('erase');
-  
-  const resetButton = document.createElement('button');
-  resetButton.textContent = 'RESET';
-  resetButton.classList.add('reset')
-  
-  buttonContainer.append(eraseButton, resetButton)
-  grid.insertAdjacentElement('afterend', buttonContainer);
-
-  resetButton.addEventListener('click', () => {
-    const coloredCells = document.querySelectorAll('.color')
-    console.log(coloredCells)
-    // coloredCells.classList.remove('color')
-  })
-
-})
 
 //add button to clear & restart
-  //change size of grid with function that takes number (input?) 
-
+  //change size of grid with function that takes number (inpu
